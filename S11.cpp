c329a99363ff9224fc3697fc861109832e98be87
Sharunienko <sarunenkoila@gmail.com>
2025-10-16 15:30:40 +0800

    Create S11.cpp

---
+++ S11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+использование пространство имён std;
+
+main int() {
+    setlocale(LC_ALL, "RU");
+    int i = 0;
+    while (i <= 100) {
+        если (i % 3 == 0) {
+ cout << i << "\t";
+        }
+ я++;
+    }
+    0 возврат;
+}
