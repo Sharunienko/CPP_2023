d3023c605dd373c86211b7eb4c5aacc2bdf49a0d
Sharunienko <sarunenkoila@gmail.com>
2025-10-16 15:36:10 +0800

    Update S02.cpp

--- S02.cpp
+++ S02.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-использование пространство имён std;
+using namespace std;
 
-main int()
+int main()
 {
     setlocale(LC_ALL, "RU");
 
@@ -12,30 +12,30 @@ main int()
     int e = -17, f = 5;
     int g = 17, h = -5;
 
- cout << "Деление нацело и остаток от деления" << endl << endl;
+    cout << "Деление на цело и остаток от деления" << endl << endl;
 
     // Примеры
- cout << "Примеры:" << endl;
- cout << a << " / " << b << " = " << (a / b) << endl;
- cout << a << " % " << b << " = " << (a % b) << endl;
- cout << "Проверка: " << b << " * " << (a / b) << " + " << (a % b) << " = " << b * (a / b) + (a % b) << endl;
- cout << endl;
+    cout << "Примеры:" << endl;
+    cout << a << " / " << b << " = " << (a / b) << endl;
+    cout << a << " % " << b << " = " << (a % b) << endl;
+    cout << "Проверка: " << b << " * " << (a / b) << " + " << (a % b) << " = " << b * (a / b) + (a % b) << endl;
+    cout << endl;
 
- cout << c << " / " << d << " = " << (c / d) << endl;
- cout << c << " % " << d << " = " << (c % d) << endl;
- cout << "15 / 4 = " << (15 / 4) << endl;
- cout << "15 % 4 = " << (15 % 4) << endl;
- cout << "8 / 2 = " << (8 / 2) << endl;
- cout << "8 % 2 = " << (8 % 2) << endl;
- cout << endl;
+    cout << c << " / " << d << " = " << (c / d) << endl;
+    cout << c << " % " << d << " = " << (c % d) << endl;
+    cout << "15 / 4 = " << (15 / 4) << endl;
+    cout << "15 % 4 = " << (15 % 4) << endl;
+    cout << "8 / 2 = " << (8 / 2) << endl;
+    cout << "8 % 2 = " << (8 % 2) << endl;
+    cout << endl;
 
     // Отрицательные числа
- cout << "Отрицательные числа:" << endl;
- cout << e << " / " << f << " = " << (e / f) << endl;
- cout << e << " % " << f << " = " << (e % f) << endl;
- cout << g << " / " << h << " = " << (g / h) << endl;
- cout << g << " % " << h << " = " << (g % h) << endl;
- cout << endl;
+    cout << "Отрицательные числа:" << endl;
+    cout << e << " / " << f << " = " << (e / f) << endl;
+    cout << e << " % " << f << " = " << (e % f) << endl;
+    cout << g << " / " << h << " = " << (g / h) << endl;
+    cout << g << " % " << h << " = " << (g % h) << endl;
+    cout << endl;
 
-    0 возврат;
+    return 0;
 }
