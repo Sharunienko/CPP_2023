7c0733189676e4a877b34811c193899a511a8e16
Sharunienko <sarunenkoila@gmail.com>
2025-12-08 22:04:52 +0800

    Update M01.cpp

--- M01.cpp
+++ M01.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "RU");
- cout << "Таблица Пифагора (10x10):" << endl;
- cout << "\t"; // Отступ для первого столбца
+    cout << "Таблица Пифагора (10x10):" << endl;
+    cout << "\t"; // Отступ для первого столбца
     
     // Вывод заголовков столбцов
     for (int col = 1; col <= 10; col++) {
- cout << col << "\t";
+        cout << col << "\t";
     }
- cout << endl;
+    cout << endl;
     
     // Вывод строк таблицы
     for (int row = 1; row <= 10; row++) {
- cout << row << "\t"; // Заголовок строки
-        for (int col = 1; col <= 10; col++) {
- cout << row * col << "\t";
+        cout << row << "\t"; // Заголовок строки
+    for (int col = 1; col <= 10; col++) {
+        cout << row * col << "\t";
         }
- cout << endl;
+    cout << endl;
     }
     return 0;
 }
